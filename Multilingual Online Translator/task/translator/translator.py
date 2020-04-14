1ca1d43372453a914b4e32e28d547eea76236818
augrak <augrak@yandex.ru>
2020-04-14 15:41:26 +0300

    Added command line mode

--- Multilingual Online Translator/task/translator/translator.py
+++ Multilingual Online Translator/task/translator/translator.py
@@ -1,5 +1,6 @@
 from requests import get
 from bs4 import BeautifulSoup
+from sys import argv
 
 
 def parse(language, word):
@@ -15,24 +16,25 @@ def parse(language, word):
     return translation_list, example_list
 
 
-def get_translate(language, word, number, out_lang):
+def get_translate(language, word, number):
     translation_list, example_list = parse(language, word)
+    out_lang = language.replace('/', '').split('-')[1]
     if number == 1:
         with open(f'/Users/aleksandrugrak/PycharmProjects/Multilingual Online Translator/'
                   f'Multilingual Online Translator/task/{word}.txt', 'a') as file:
-            file.write(f'{lang_dict[out_lang]} Translations:\n')
+            file.write(f'{out_lang.capitalize()} Translations:\n')
             file.write(translation_list[1] + '\n')
-            file.write(f'\n{lang_dict[out_lang]} Examples:\n')
+            file.write(f'\n{out_lang.capitalize()} Examples:\n')
             file.write(example_list[0] + '\n')
-            if out_lang == 13:
+            if out_lang.lower() == 'turkish':
                 file.write(example_list[1])
             else:
                 file.write(example_list[1] + '\n\n\n')
 
-    print(f'\n{lang_dict[out_lang]} Translations:')
+    print(f'\n{out_lang.capitalize()} Translations:')
     for i in translation_list[1:number + 1]:
         print(i)
-    print(f'\n{lang_dict[out_lang]} Examples:')
+    print(f'\n{out_lang.capitalize()} Examples:')
     counter = 0
     for i in example_list[:number * 2]:
         print(i)
@@ -43,21 +45,26 @@ def get_translate(language, word, number, out_lang):
 
 
 if __name__ == "__main__":
-    print('''Hello, welcome to the translator. Translator supports: 
-    1. Arabic\n2. German\n3. English\n4. Spanish\n5. French\n6. Hebrew\n7. Japanese
-8. Dutch\n9. Polish\n10. Portuguese\n11. Romanian\n12. Russian\n13. Turkish''')
-    inp_lang = int(input('Type the number of your language:\n'))
-    out_lang = int(input("Type the number of a language you want "
-                         "to translate to or '0' to translate to all languages:\n"))
-    word = input('Type the word you want to translate:\n')
-    lang_dict = {1: 'Arabic', 2: 'German', 3: 'English', 4: 'Spanish', 5: 'French', 6: 'Hebrew', 7: 'Japanese',
-                 8: 'Dutch', 9: 'Polish', 10: 'Portuguese', 11: 'Romanian', 12: 'Russian', 13: 'Turkish'}
-
-    if out_lang == 0:
-        for key, value in lang_dict.items():
-            if key != inp_lang:
-                language = lang_dict[inp_lang].lower() + '-' + lang_dict[key].lower() + '/'
-                get_translate(language, word, 1, key)
+    lang_dict = {0: 'all', 1: 'arabic', 2: 'german', 3: 'english', 4: 'spanish', 5: 'french', 6: 'hebrew',
+                 7: 'japanese', 8: 'dutch', 9: 'polish', 10: 'portuguese', 11: 'romanian', 12: 'russian', 13: 'turkish'}
+    if len(argv) == 4:
+        inp_lang, out_lang, word = argv[1], argv[2], argv[3]
     else:
-        language = lang_dict[inp_lang].lower() + '-' + lang_dict[out_lang].lower() + '/'
-        get_translate(language, word, 5, out_lang)
+        print('''Hello, welcome to the translator. Translator supports: 
+1. Arabic\n2. German\n3. English\n4. Spanish\n5. French\n6. Hebrew\n7. Japanese
+8. Dutch\n9. Polish\n10. Portuguese\n11. Romanian\n12. Russian\n13. Turkish''')
+        inp_lang = int(input('Type the number of your language:\n'))
+        out_lang = int(input("Type the number of a language you want "
+                             "to translate to or '0' to translate to all languages:\n"))
+        word = input('Type the word you want to translate:\n')
+        inp_lang = lang_dict[inp_lang]
+        out_lang = lang_dict[out_lang]
+
+    if out_lang == 'all':
+        for key, value in lang_dict.items():
+            if lang_dict[key] != inp_lang and lang_dict[key] != 'all':
+                language = inp_lang.lower() + '-' + lang_dict[key].lower() + '/'
+                get_translate(language, word, 1)
+    else:
+        language = inp_lang.lower() + '-' + out_lang.lower() + '/'
+        get_translate(language, word, 5)
